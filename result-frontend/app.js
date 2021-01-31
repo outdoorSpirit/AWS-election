4912fdd9a93eddd2fdd3ea8cefa6b9db2675dba0
Aleksandr Volochnev <a.volochnev@gmail.com>
2021-01-31 23:49:48 +0100

    Results

--- result-frontend/app.js
+++ result-frontend/app.js
@@ -1,39 +1,49 @@
-var app = angular.module('vote-results', []);
+var bg1 = $('#background-stats-1');
+var bg2 = $('#background-stats-2');
+var valueA = $('#a'); 
+var valueB = $('#b'); 
+var total = $('#result'); 
 
-var bg1 = document.getElementById('background-stats-1');
-var bg2 = document.getElementById('background-stats-2');
-
-app.controller('statsCtrl', function($scope){
-  var animateStats = function(a,b){
-    if(a+b>0){
-      var percentA = a/(a+b)*100;
-      var percentB = 100-percentA;
-      bg1.style.width= percentA+"%";
-      bg2.style.width = percentB+"%";
+function animateStats (a,b){
+    if(a + b > 0){
+        var percentA = a/(a+b)*100;
+        var percentB = 100-percentA;
+        bg1.width((percentA-0.3)+"%");
+        bg2.width(percentB+"%");
     }
-  };
+}
 
-  $scope.aPercent = 50;
-  $scope.bPercent = 50;
+function updateScores (){
+    $.get("https://kmti6g8um6.execute-api.eu-central-1.amazonaws.com/my-vote", null, function(result,status){
+        if ("success" == status) {
+            console.log(result);
 
-  var updateScores = function(){
-    //socket.on('scores', function (json) {
-       //data = JSON.parse(json);
-       //var a = parseInt(data.a || 0);
-       //var b = parseInt(data.b || 0);
-       a = 3; b = 5;
-       animateStats(a, b);
+            data = JSON.parse(result);
+            var a = parseInt(data.a || 0);
+            var b = parseInt(data.b || 0);
+            
+            animateStats(a, b);
+        
+            if(a + b > 0){
+                valueA.text(Math.round((a/(a+b) * 100) * 10) / 10 + "%");
+                valueB.text(Math.round((b/(a+b) * 100) * 10) / 10 + "%");
+                total.text("Всего голосов: " + (a + b))
+            }
+        } else {
+            console.log(result);
+        }
+    });
+}
 
-       $scope.$apply(function() {
-         if(a + b > 0){
-           $scope.aPercent = a/(a+b) * 100;
-           $scope.bPercent = b/(a+b) * 100;
-           $scope.total = a + b
-         }
-      });
-    //});
-  };
-
-  document.body.style.opacity=1;
-  updateScores();
+$.ajaxSetup({
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 });
+document.body.style.opacity=1;
+updateScores();
+
+setInterval(function() {
+    updateScores();
+}, 3000);
