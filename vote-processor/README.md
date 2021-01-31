b4a0a867b94055af361bf460115ab412420a4b2b
Aleksandr Volochnev <a.volochnev@gmail.com>
2021-01-31 19:05:01 +0100

    Processor fixes

--- vote-processor/README.md
+++ vote-processor/README.md
@@ -1,7 +1,55 @@
 # Запуск
 
+## Локальный Запуск
 ```
-python -m pip install --user boto3 -r requirements.txt
+python -m pip install --user boto3
 chmod +x processor.py
 ./processor.py
+```
+
+## Запуск на инстансе EC2
+```
+curl https://raw.githubusercontent.com/HadesArchitect/MyVoteAWS/main/vote-processor/processor.py > processor.py
+chmod +x processor.py
+sudo yum install python-pip
+python -m pip install --user boto3
+./processor.py
+```
+
+# Зависимости
+
+## Политики
+
+### Политика для SQS
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sqs:GetQueueUrl",
+                "sqs:ReceiveMessage",
+                "sqs:DeleteMessage"
+            ],
+            "Resource": "arn:aws:sqs:*:ACCOUNT:QUEUENAME"
+        }
+    ]
+}
+```
+
+### Политика для DynamoDB
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+             "Effect": "Allow",
+            "Action": "dynamodb:PutItem",
+            "Resource": "arn:aws:dynamodb:*:ACCOUNT:table/TABLENAME"
+        }
+    ]
+}
 ```
--- vote-processor/processor.py
+++ vote-processor/processor.py
@@ -3,11 +3,12 @@
 import boto3
 import json
 import logging
+import exceptions
 import sys
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-queue = boto3.resource("sqs").get_queue_by_name(QueueName="my-vote")
+queue = boto3.resource('sqs', region_name='eu-central-1').get_queue_by_name(QueueName="my-vote")
 table = boto3.resource('dynamodb', region_name='eu-central-1').Table('Votes')
 
 def process_message(message):
@@ -18,21 +19,26 @@ def process_message(message):
         logging.info("Voter: %s, Vote: %s", voter, vote)
         store_vote(voter, vote)
         message.delete()
-    except:
-        logging.error(sys.exc_info()[0])
+    except Exception as e:
+        logging.error("Failed to process message")
+        logging.error(str(e))
 
 def store_vote(voter, vote):
-    response = table.put_item(
-       Item={
-            'voter': voter,
-            'vote': vote,
-        }
-    )
+    try:
+        response = table.put_item(
+           Item={'voter': voter, 'vote': vote}
+        )
+    except:
+        logging.error("Failed to store message")
+        raise
 
 if __name__ == "__main__":
     while True:
         try:
             messages = queue.receive_messages()
+        except exceptions.KeyboardInterrupt:
+           logging.info("Stopping...")
+           break
         except:
             logging.error(sys.exc_info()[0])
             continue
