cb03341efa4994122e31bffbb3b4630a9f470be2
Aleksandr Volochnev <a.volochnev@gmail.com>
2021-02-01 01:28:47 +0100

    Results backend

--- result-backend/results.py
+++ result-backend/results.py
@@ -1,15 +1,13 @@
-import json
 import logging
 import boto3
 
 logging.getLogger().setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-
-#    try:
-#        get_votes()
-#    except Exception as e:
-#        logging.error(e)
-#        return {'statusCode': 500, 'body': '{"status": "error"}'} 
+    dynamodb = boto3.client('dynamodb')
+    count = dynamodb.get_item(TableName='Votes', Key={'voter':{'S': 'count'}})
     
-    return {'statusCode': 200, 'body': '{"a": 3, "b": 5}'}
+    a = count["Item"]["a"]["N"]
+    b = count["Item"]["b"]["N"]
+
+    return {'statusCode': 200, 'body': '{"a": ' + a + ', "b": ' + b + '}'}
