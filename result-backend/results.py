c866733ad3622d75d8ef0aa5cd59a014bb26ce22
Junjie529 <junjie@zhuotong.com.hk>
2023-01-17 17:52:16 +0500

    Update results.py

--- result-backend/results.py
+++ result-backend/results.py
@@ -4,10 +4,15 @@ import boto3
 logging.getLogger().setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    
     dynamodb = boto3.client('dynamodb')
     count = dynamodb.get_item(TableName='Votes', Key={'voter':{'S': 'count'}})
     
     a = count["Item"]["a"]["N"]
     b = count["Item"]["b"]["N"]
+    print("a: " + str(a))
+    print("b: " + str(b))
 
     return {'statusCode': 200, 'body': '{"a": ' + a + ', "b": ' + b + '}'}
+
+
